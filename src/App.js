1347ef8c93c93e50ee80811f00c5fe18a782d8b9
Kimberly <83323970+kimbcheh@users.noreply.github.com>
2021-06-15 09:41:04 +1000

    feat: set up initial structure (#1)
    * add prettier config file
    
    * add material ui package and roboto font
    
    * add basic placeholder structure
    
    * remove create-react-app css defaults
    
    * add grid component for flex
    
    * change roboto font import method
    
    * change tab width to 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import Main from './Components/Main'
+import { Grid } from '@material-ui/core'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ return (
+  <Grid container direction='column'>
+   <Grid item>
+    <p>Header Placeholder</p>
+   </Grid>
+   <Main />
+   <Grid item>
+    <p>Footer Placeholder</p>
+   </Grid>
+  </Grid>
+ )
 }
 
-export default App;
+export default App
---
+++ src/Components/Main.js
@@ -0,0 +1,11 @@
+function Main() {
+ return (
+  <div>
+   <p>Search Placeholder</p>
+   <p>Current Placeholder</p>
+   <p>Forecast Placeholder</p>
+  </div>
+ )
+}
+
+export default Main
