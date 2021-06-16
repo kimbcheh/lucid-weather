45a29f6eb4764211a7ca9679cc760f8b0a23ed44
Kimberly <kimbcheh@gmail.com>
2021-06-16 11:51:04 +1000

    add map function to render list of data

--- src/Components/ForecastWeather.js
+++ src/Components/ForecastWeather.js
@@ -1,10 +1,28 @@
+import { Grid, Typography } from '@material-ui/core'
 function ForecastWeather({ data }) {
  const forecastArr = data.daily
 
  return (
-  <div>
-   <p>ForecastWeather component</p>
-  </div>
+  <Grid container>
+   {forecastArr.map((day) => {
+    return (
+     <Grid container key={day.dt}>
+      <Grid item>
+       <p>Date: {day.dt}</p>
+      </Grid>
+      <Grid item>
+       <p>Desc: {day.weather[0].description}</p>
+      </Grid>
+      <Grid item>
+       <p>Max: {day.temp.max}</p>
+      </Grid>
+      <Grid item>
+       <p>Min: {day.temp.min}</p>
+      </Grid>
+     </Grid>
+    )
+   })}
+  </Grid>
  )
 }
 
