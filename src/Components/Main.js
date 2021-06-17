32447d5bd1f145f069fdb9c9ee82d52418db94d0
Kimberly <kimbcheh@gmail.com>
2021-06-17 11:02:36 +1000

    add error state when fetching data fails

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -8,9 +8,10 @@ import { CircularProgress } from '@material-ui/core'
 const key = process.env.REACT_APP_WEATHER_API_KEY
 
 function Main() {
- const [city, setCity] = useState('Melbourne')
+ const [city, setCity] = useState()
  const [data, setData] = useState()
  const [isLoading, setIsLoading] = useState(false)
+ const [isError, setIsError] = useState(false)
 
  const initialRender = useRef(true)
 
@@ -20,18 +21,25 @@ function Main() {
     initialRender.current = false
    } else {
     setIsLoading(true)
+    setIsError(false)
     console.log('loading')
-    const coordinatesData = await axios.get(
-     `http://api.openweathermap.org/geo/1.0/direct?q=${city},AU&limit=1&appid=${key}`
-    )
-    const lon = coordinatesData.data[0].lon
-    const lat = coordinatesData.data[0].lat
+    try {
+     const coordinatesData = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city},AU&limit=1&appid=${key}`
+     )
+     const lon = coordinatesData.data[0].lon
+     const lat = coordinatesData.data[0].lat
 
-    const weatherData = await axios.get(
-     `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=${key}`
-    )
-    setData(weatherData.data)
-    console.log(weatherData.data)
+     const weatherData = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=${key}`
+     )
+     setData(weatherData.data)
+     console.log(weatherData.data)
+    } catch (error) {
+     setIsError(true)
+     console.error(error)
+     console.log('hi im an error')
+    }
     setIsLoading(false)
     console.log('not loading')
    }
@@ -42,9 +50,12 @@ function Main() {
  return (
   <div>
    <SearchBar onSearch={setCity} />
+   {isError && <p>Sorry, something went wrong!</p>}
    {isLoading && <CircularProgress color='primary' />}
-   {!isLoading && data && <CurrentWeather data={data} city={city} />}
-   {!isLoading && data && <ForecastWeather data={data} />}
+   {!isLoading && !isError && data && (
+    <CurrentWeather data={data} city={city} />
+   )}
+   {!isLoading && !isError && data && <ForecastWeather data={data} />}
   </div>
  )
 }
