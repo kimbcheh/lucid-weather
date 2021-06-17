2c502b24d8e9ea32b739de054e245bf216c12554
Kimberly <83323970+kimbcheh@users.noreply.github.com>
2021-06-17 11:55:13 +1000

    feat: add loading and error states (#6)
    * add isLoading state so weather won't render when loading
    
    * add spinner component during loading
    
    * stop API call on first render
    
    * add error state when fetching data fails
    
    * remove error and loading testing console.log messages
    
    * add variable to conditionally render weather components

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,37 +1,58 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import axios from 'axios'
 import SearchBar from './SearchBar'
 import CurrentWeather from './CurrentWeather'
 import ForecastWeather from './ForecastWeather'
+import { CircularProgress } from '@material-ui/core'
 
 const key = process.env.REACT_APP_WEATHER_API_KEY
 
 function Main() {
- const [city, setCity] = useState('Melbourne')
+ const [city, setCity] = useState()
  const [data, setData] = useState()
+ const [isLoading, setIsLoading] = useState(false)
+ const [isError, setIsError] = useState(false)
+
+ const initialRender = useRef(true)
 
  useEffect(() => {
   const fetchData = async () => {
-   const coordinatesData = await axios.get(
-    `http://api.openweathermap.org/geo/1.0/direct?q=${city},AU&limit=1&appid=${key}`
-   )
-   const lon = coordinatesData.data[0].lon
-   const lat = coordinatesData.data[0].lat
+   if (initialRender.current) {
+    initialRender.current = false
+   } else {
+    setIsLoading(true)
+    setIsError(false)
+    try {
+     const coordinatesData = await axios.get(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${city},AU&limit=1&appid=${key}`
+     )
+     const lon = coordinatesData.data[0].lon
+     const lat = coordinatesData.data[0].lat
 
-   const weatherData = await axios.get(
-    `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=${key}`
-   )
-   setData(weatherData.data)
-   console.log(weatherData.data)
+     const weatherData = await axios.get(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly,alerts&units=metric&appid=${key}`
+     )
+     setData(weatherData.data)
+     console.log(weatherData.data)
+    } catch (error) {
+     setIsError(true)
+     console.error(error)
+    }
+    setIsLoading(false)
+   }
   }
   fetchData()
  }, [city])
 
+ const safeRender = !isLoading && !isError && data
+
  return (
   <div>
    <SearchBar onSearch={setCity} />
-   {data && city && <CurrentWeather data={data} city={city} />}
-   {data && city && <ForecastWeather data={data} />}
+   {isError && <p>Sorry, something went wrong!</p>}
+   {isLoading && <CircularProgress color='primary' />}
+   {safeRender && <CurrentWeather data={data} city={city} />}
+   {safeRender && <ForecastWeather data={data} />}
   </div>
  )
 }
