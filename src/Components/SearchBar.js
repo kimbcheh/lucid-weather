bc0b6201d1c12d2c776c0503b9f00228e521da74
kimbcheh <kimbcheh@gmail.com>
2021-06-21 14:19:27 +1000

    add basic sc to SearchBar

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,4 +1,45 @@
 import { useState } from 'react'
+import styled from 'styled-components'
+
+const SearchContainer = styled.div`
+ background-color: seashell;
+ display: flex;
+ flex-direction: column;
+ padding-bottom: 1rem;
+ padding-top: 1rem;
+ border-top: 1px solid black;
+ border-bottom: 1px solid black;
+`
+
+const SearchLabel = styled.label`
+ background-color: teal;
+ font-size: 1rem;
+ margin: 0.25rem;
+`
+
+const SearchInput = styled.input`
+ margin: 0.25rem;
+ padding: 0.5rem;
+ border-radius: 0;
+ border: 1px solid black;
+ &:focus {
+  outline: 2px solid pink;
+ }
+`
+
+const SearchButton = styled.button`
+ margin: 0.25rem;
+ background-color: black;
+ border: none;
+ color: white;
+ padding: 1rem;
+ text-align: center;
+ font-size: 1rem;
+ cursor: pointer;
+ &:focus {
+  outline: 2px solid pink;
+ }
+`
 
 function SearchBar({ onSearch }) {
  const [searchTerm, setSearchTerm] = useState()
@@ -14,16 +55,16 @@ function SearchBar({ onSearch }) {
  }
 
  return (
-  <div>
-   <label for='cityName'>Enter an Australian city:</label>
-   <input
+  <SearchContainer>
+   <SearchLabel for='cityName'>Enter an Australian city:</SearchLabel>
+   <SearchInput
     type='text'
     id='cityName'
     name='cityName'
     onChange={changeHandler}
-   ></input>
-   <button onClick={clickHandler}>Search</button>
-  </div>
+   ></SearchInput>
+   <SearchButton onClick={clickHandler}>Search</SearchButton>
+  </SearchContainer>
  )
 }
 
