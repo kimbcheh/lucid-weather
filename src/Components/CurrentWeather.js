d18289208d67f41d5604e68b48ee9c40a39b43b9
kimbcheh <kimbcheh@gmail.com>
2021-06-21 13:57:40 +1000

    add padding, margin and heading to CurrentWeather

--- src/Components/CurrentWeather.js
+++ src/Components/CurrentWeather.js
@@ -3,16 +3,34 @@ import styled from 'styled-components'
 
 const CurrentContainer = styled.div`
  background-color: salmon;
+ padding-bottom: 1rem;
+ padding-top: 1rem;
+ border-top: 1px solid black;
+ border-bottom: 1px solid black;
 `
 
 const CurrentTemp = styled.p`
  background-color: slategrey;
  font-size: 3rem;
+ margin: 0.25rem;
 `
 
 const CurrentDesc = styled.p`
  background-color: violet;
  font-size: 2.25rem;
+ margin: 0.25rem;
+`
+
+const CurrentItem = styled.p`
+ background-color: powderblue;
+ margin: 0.25rem;
+`
+
+const Heading = styled.h2`
+ background-color: pink;
+ font-size: 1rem;
+ margin: 0.25rem;
+ text-decoration: underline;
 `
 
 function CurrentWeather({ data }) {
@@ -21,16 +39,17 @@ function CurrentWeather({ data }) {
 
  return (
   <CurrentContainer>
+   <Heading>CURRENTLY:</Heading>
    <CurrentTemp>{current.temp}°</CurrentTemp>
    <CurrentDesc>Enjoy your {current.weather[0].description} today.</CurrentDesc>
-   <p>
+   <CurrentItem>
     TEMP: ↑ {daily.temp.max}° ↓ {daily.temp.min}°
-   </p>
-   <p>
+   </CurrentItem>
+   <CurrentItem>
     RAIN: {daily.pop}% {daily.rain && `/ ${daily.rain}mm`}
-   </p>
-   <p>SUNRISE: {convertTime(current.sunrise)}</p>
-   <p>SUNSET: {convertTime(current.sunset)}</p>
+   </CurrentItem>
+   <CurrentItem>SUNRISE: {convertTime(current.sunrise)}</CurrentItem>
+   <CurrentItem>SUNSET: {convertTime(current.sunset)}</CurrentItem>
   </CurrentContainer>
  )
 }
