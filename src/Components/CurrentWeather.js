7402a8322a768e35b87a4db45da0762dd9f044c3
kimbcheh <kimbcheh@gmail.com>
2021-06-21 11:58:47 +1000

    set up basic sc for CurrentWeather

--- src/Components/CurrentWeather.js
+++ src/Components/CurrentWeather.js
@@ -1,34 +1,37 @@
 import { convertTime } from './DateFunctions'
+import styled from 'styled-components'
+
+const CurrentContainer = styled.div`
+ background-color: salmon;
+`
+
+const CurrentTemp = styled.p`
+ background-color: slategrey;
+ font-size: 3rem;
+`
+
+const CurrentDesc = styled.p`
+ background-color: violet;
+ font-size: 2.25rem;
+`
 
 function CurrentWeather({ data }) {
  let current = data.current
  let daily = data.daily[0]
 
  return (
-  <div>
-   <div>
-    <h2>{current.temp}°</h2>
-   </div>
-   <div>
-    <h4>Enjoy your {current.weather[0].description} today.</h4>
-   </div>
-   <div>
-    <div>
-     <p>
-      TEMP: ↑ {daily.temp.max}° ↓ {daily.temp.min}°
-     </p>
-    </div>
-    <div>
-     <p>
-      RAIN: {daily.pop}% {daily.rain && `/ ${daily.rain}mm`}
-     </p>
-    </div>
-    <div>
-     <p>SUNRISE: {convertTime(current.sunrise)}</p>
-     <p>SUNSET: {convertTime(current.sunset)}</p>
-    </div>
-   </div>
-  </div>
+  <CurrentContainer>
+   <CurrentTemp>{current.temp}°</CurrentTemp>
+   <CurrentDesc>Enjoy your {current.weather[0].description} today.</CurrentDesc>
+   <p>
+    TEMP: ↑ {daily.temp.max}° ↓ {daily.temp.min}°
+   </p>
+   <p>
+    RAIN: {daily.pop}% {daily.rain && `/ ${daily.rain}mm`}
+   </p>
+   <p>SUNRISE: {convertTime(current.sunrise)}</p>
+   <p>SUNSET: {convertTime(current.sunset)}</p>
+  </CurrentContainer>
  )
 }
 
