d638f130c748c7100cc48c032ed5f0f0dce45414
kimbcheh <kimbcheh@gmail.com>
2021-06-22 14:34:35 +1000

    add ErrorMessage component

---
+++ src/Components/ErrorMessage.js
@@ -0,0 +1,11 @@
+function ErrorMessage() {
+ return (
+  <div className='cont cont--flex-row cont--justify-center'>
+   <p className='body'>
+    Sorry, something went wrong! Try a different city or try again later...
+   </p>
+  </div>
+ )
+}
+
+export default ErrorMessage
