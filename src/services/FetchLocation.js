59115129a341b7a59e7634cadc95ea4101de27af
kimbcheh <kimbcheh@gmail.com>
2021-06-23 21:49:09 +1000

    change API URL endpoint in FetchLocation

--- src/services/FetchLocation.js
+++ src/services/FetchLocation.js
@@ -4,7 +4,7 @@ const key = process.env.REACT_APP_WEATHER_API_KEY
 
 const fetchLocation = (city) => {
  return axios.get(
-  `http://api.openweathermap.org/geo/1.0/direct?q=${city},AU&limit=1&appid=${key}`,
+  `https://api.openweathermap.org/geo/1.0/direct?q=${city},AU&limit=1&appid=${key}`,
   { timeout: 5000 }
  )
 }
