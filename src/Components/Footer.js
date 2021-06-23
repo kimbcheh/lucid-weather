a8971b2c8a5fb966cd3ac5ee03020dbeb55d584f
kimbcheh <kimbcheh@gmail.com>
2021-06-23 12:30:43 +1000

    adjust spacing for footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import '../styles/styles.css'
 
 function Footer() {
  return (
-  <div className='cont cont--align-center cont--border cont--flex-row cont--footer'>
+  <div className='cont cont--align-center cont--border cont--flex-column cont--footer'>
    <p className='subheading'>&#128171; Made by Kimberly Cheh. </p>
   </div>
  )
