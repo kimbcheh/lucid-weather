00fdfbba22d8d4003511ff1b832b75b83d75b091
kimbcheh <kimbcheh@gmail.com>
2021-06-23 12:50:32 +1000

    add semantic elements to Footer component

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,9 +2,9 @@ import '../styles/styles.css'
 
 function Footer() {
  return (
-  <div className='cont cont--align-center cont--border cont--flex-column cont--footer'>
-   <p className='subheading'>&#128171; Made by Kimberly Cheh. </p>
-  </div>
+  <footer className='cont cont--align-center cont--border cont--flex-column cont--footer'>
+   <small className='subheading'>&#128171; Made by Kimberly Cheh. </small>
+  </footer>
  )
 }
 
